507bc99350e7a57ab3c0b44f29ed2fd86f736b57
Alex <alex.fraga@estudante.ifto.edu.br>
2025-06-06 15:31:12 -0300

    Corrige cálculo do perímetro para fechar o polígono, no método tipoPoligonos.

--- src/main/java/com/example/PoligonosApp.java
+++ src/main/java/com/example/PoligonosApp.java
@@ -112,49 +112,14 @@ public class PoligonosApp extends Application {
                 .toList();
     }
 
-    /**
-     * Calcula o perímetro de cada polígono.
-     * O perímetro é a soma da distância entre cada {@link Point} (x,y) do {@link Polygon}.
-     * Se você pensar em um polígono como um quadrado, o perímetro representa a distância que você percorreria
-     * se andasse ao redor da borda do quadrado, do ponto inicial até o último ponto.
-     *
-     * <p>Este método é mais complexo. A implementação dele deve usar a operação {@link Stream#flatMap(Function)} que
-     * percorre os itens de {@link #pontosPoligonos} (cada item representando um polígono).
-     * O que queremos obter de cada item (lista de pontos) é a soma da distância entre cada ponto.
-     * </p>
-     *
-     * <p>O record {@link Point} (veja javadoc dele para mais detalhes)
-     * possui um construtor {@link Point#Point(Point, Point)} que recebe 2 pontos,
-     * cria um novo que contém:
-     * (i) as coordenadas do segundo ponto e
-     * (ii) a distância entre os pontos no atributo {@link Point#distance} (acessado pelo método getter {@link Point#distance()}).
-     * Tal construtor já soma a distância entre p1 e p2 com a distância do p1 com o ponto anterior a ele.</p>
-     *
-     * <p>Assim, você precisaria percorrer todos os pontos de um polígono, pegar um par de pontos e passar
-     * para tal construtor. Pegar o terceiro ponto e o ponto criado na chamada do construtor anteriormente,
-     * e chamar o construtor novamente, passando estes 2 pontos, até que você chegue ao último ponto
-     * do polígono.
-     * Mas é exatamente isso que o reduce faz. Então para obter um ponto final contendo a soma da distância entre
-     * todos os pontos de um polígono, você deve usar o método reduce no parâmetro recebido no flatMap.</p>
-     *
-     * <p>No entanto, considere que temos um triângulo. Se utilizamos o método {@link Stream#reduce(BinaryOperator)},
-     * ele permitirá calcular a distância entre os pontos A -> B e B -> C somente.
-     * Mas para calcular o perímetro, precisamos fechar os pontos, obtendo também a distância entre C -> A.
-     * Assim, podemos começar do A (1o ponto) e indicar que o ponto anterior é o C.
-     * Apesar de iniciar do A, estaríamos calculando as distâncias entre C -> A, A -> B e B -> C, fechando
-     * todos os pontos. Com a versão do reduce indicada acima, não será possível fazer isso.
-     * Desta forma, a versão {@link Stream#reduce(Object, BinaryOperator)} deve ser usada no lugar.
-     * Leia o JavaDoc de tal método para mais detalhes.</p>
-     *
-     * <p>Após o flatMap, você vai ter um único ponto para cada polígono, que representa o último ponto encontrado
-     * e contém o perímetro do polígono, que pode ser acessado por {@link Point#distance()}.
-     * Desta forma, basta retornar este resultado como uma nova lista de {@link Double}.</p>
-     *
-     * @return uma lista contendo o perímetro de cada polígono
-     */
-    protected List<Double> perimetros(){
-        // TODO Apague esta linha e a próxima e implemente seu código
-        return List.of();
+    protected List<Double> perimetros() {
+        return pontosPoligonos.stream()
+                .map(listaPontos -> {
+                    final var streamFechado = Stream.concat(listaPontos.stream(), Stream.of(listaPontos.get(0)));
+                    return streamFechado.reduce((p1, p2) -> new Point(p1, p2)).get();
+                })
+                .map(Point::distance)
+                .toList();
     }
 }
 
